4494a5e08fc43e38a65fe54d63625189a2e271eb
Vishal <vishaa369@gmail.com>
2025-09-02 23:02:44 +0530

    Update on Service Page

--- src/components/services/SingleServiceV5.tsx
+++ src/components/services/SingleServiceV5.tsx
@@ -1,46 +1,42 @@
-import { Link } from "react-router-dom";
 import useHoverEffect from "../../hooks/useHoverEffect";
 
 interface DataType {
-    id: number;
-    number: string;
-    title: string;
-    thumb: string;
+  id: number;
+  number: string;
+  title: string;
+  thumb: string;
 }
 
 const SingleServiceV5 = ({ service }: { service: DataType }) => {
-    const { id, number, title, thumb } = service;
+  const { number, title, thumb } = service;
 
-    const { handleMouseMove, handleMouseLeave } = useHoverEffect();
+  const { handleMouseMove, handleMouseLeave } = useHoverEffect();
 
-    return (
-        <>
-            <li>
-                <Link
-                    to={`/service-details/${id}`}
-                    className="image-hover-item"
-                    onMouseMove={handleMouseMove}
-                    onMouseLeave={handleMouseLeave}
-                >
-                    <div className="item-content">
-                        <span>{number}</span>
-                        <Link to={`/service-details/${id}`}>
-                            <h2>{title}</h2>
-                        </Link>
-                    </div>
-                    <div className="image-hover-wrapper">
-                        <img
-                            className="image-hover-placeholder"
-                            src={`/assets/img/portfolio/${thumb}`}
-                            alt="Image Not Found"
-                            width={600}
-                            height={600}
-                        />
-                    </div>
-                </Link>
-            </li>
-        </>
-    );
+  return (
+    <>
+      <li>
+        <div
+          className="image-hover-item"
+          onMouseMove={handleMouseMove}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className="item-content">
+            <span>{number}</span>
+            <h2>{title}</h2>
+          </div>
+          <div className="image-hover-wrapper">
+            <img
+              className="image-hover-placeholder"
+              src={`/assets/img/portfolio/${thumb}`}
+              alt="Image Not Found"
+              width={600}
+              height={600}
+            />
+          </div>
+        </div>
+      </li>
+    </>
+  );
 };
 
 export default SingleServiceV5;
--- src/hooks/useHoverEffect.tsx
+++ src/hooks/useHoverEffect.tsx
@@ -1,27 +1,35 @@
-import { useCallback } from 'react';
+import { useCallback } from "react";
 
 const useHoverEffect = () => {
-    const handleMouseMove = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
-        const wrapper = e.currentTarget.querySelector('.image-hover-wrapper') as HTMLElement;
-        const image = e.currentTarget.querySelector('.image-hover-placeholder') as HTMLElement;
-        if (wrapper && image) {
-            wrapper.style.opacity = '1';
-            wrapper.style.transform = 'translateX(-50px) rotate(-8deg)';
-            image.style.transform = 'scale(1, 1)';
-        }
-    }, []);
+  const handleMouseMove = useCallback((e: React.MouseEvent<HTMLElement>) => {
+    const wrapper = e.currentTarget.querySelector(
+      ".image-hover-wrapper"
+    ) as HTMLElement;
+    const image = e.currentTarget.querySelector(
+      ".image-hover-placeholder"
+    ) as HTMLElement;
+    if (wrapper && image) {
+      wrapper.style.opacity = "1";
+      wrapper.style.transform = "translateX(-50px) rotate(-8deg)";
+      image.style.transform = "scale(1, 1)";
+    }
+  }, []);
 
-    const handleMouseLeave = useCallback((e: React.MouseEvent<HTMLAnchorElement>) => {
-        const wrapper = e.currentTarget.querySelector('.image-hover-wrapper') as HTMLElement;
-        const image = e.currentTarget.querySelector('.image-hover-placeholder') as HTMLElement;
-        if (wrapper && image) {
-            wrapper.style.opacity = '0';
-            wrapper.style.transform = 'translateX(100px) rotate(8deg)';
-            image.style.transform = 'scale(0.5, 0.5)';
-        }
-    }, []);
+  const handleMouseLeave = useCallback((e: React.MouseEvent<HTMLElement>) => {
+    const wrapper = e.currentTarget.querySelector(
+      ".image-hover-wrapper"
+    ) as HTMLElement;
+    const image = e.currentTarget.querySelector(
+      ".image-hover-placeholder"
+    ) as HTMLElement;
+    if (wrapper && image) {
+      wrapper.style.opacity = "0";
+      wrapper.style.transform = "translateX(100px) rotate(8deg)";
+      image.style.transform = "scale(0.5, 0.5)";
+    }
+  }, []);
 
-    return { handleMouseMove, handleMouseLeave };
+  return { handleMouseMove, handleMouseLeave };
 };
 
-export default useHoverEffect
+export default useHoverEffect;
